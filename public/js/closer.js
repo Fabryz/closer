b1ae28199bc7ec2295928f3505e16762599f8020
Fabrizio Codello <fabryz@gmail.com>
2012-03-18 23:07:20 +0100

    added players data structure, ping, markers

--- public/js/closer.js
+++ public/js/closer.js
@@ -1,19 +1,10 @@
 $(document).ready(function() {
-	var socket = new io.connect(window.location.href);
-	
-	var status = $("#status"),
-		clientId = $("#clientId"),
-		online = $("#online"),
-		tot = $("#tot"),
-		debug = $("#debug"),
-		last_coords = { lat: 0, lon: 0 , acc: 0},
-		watchid,
-		map;
-		
-	status.html("Connecting...");
-	initialize();
 
-	function initialize() {
+	function log(msg) {
+		console.log(msg);
+	}
+
+	function init() {
 		var latlng = new google.maps.LatLng(45.66629, 12.24207);
 		var myOptions = {
 			zoom: 19,
@@ -40,67 +31,117 @@ $(document).ready(function() {
 		}
 	}
 
+	function setMarker(latlng, id) {
+		var marker = new google.maps.Marker({
+			position: latlng, 
+			map: map, 
+			title: "You are here! (at least within a "+ latlng.acc +" meter radius)"
+		});
+
+		if (typeof id != undefined) {
+			var length = players.length;
+			for(var i = 0; i < length; i++) {
+				if (players[i].id == id) {
+					players[i].marker = marker;
+					if (player.id == id) {
+						player.marker = marker;
+					}
+					break;
+				}
+			}
+		}
+	}
+
+	function sendCoords(coords) {
+		socket.emit("coords", { id: player.id, lat: coords.latitude, lon: coords.longitude, acc: coords.accuracy });
+
+		$("h1").addClass("sent");
+		setTimeout(function() {
+			$("h1").removeClass("sent");
+		}, 300);
+	}
+
+	function geolocate() {
+		if (navigator.geolocation) {
+			watchid = navigator.geolocation.watchPosition(function(position) {   
+				player.lat = position.coords.latitude;
+				player.lng = position.coords.longitude;
+				player.acc = position.coords.accuracy;
+
+				$("#map_status").html(player);
+
+				var latlng = new google.maps.LatLng(player.lat, player.lng);
+				console.log(latlng);
+				map.setCenter(latlng);
+				setMarker(latlng, player.id);
+
+				sendCoords(player);
+			}, function(err) {
+				$("#map_status").html("Error: see console.");
+				console.dir(err);
+			}, { enableHighAccuracy: true, maximumAge: 30000, timeout: 10000 });
+		} else {
+			$("#map_status").html("Error: Unable to grab your current position.");
+		}
+	}
+
+	/*
+	* Main
+	*/
+
+	var socket = new io.connect(window.location.href);
+	
+	var status = $("#status"),
+		clientId = $("#clientId"),
+		online = $("#online"),
+		tot = $("#tot"),
+		debug = $("#debug"),
+		watchid,
+		map;
+
+	var player = new Player(),
+		players = [];
+
 	$(document).keyup(function(e) {
 		if (e.keyCode === 220) { //backslash
 			toggleDebug();
 		}
 	});
 
-	function setMarker(latlng) {
-		var marker = new google.maps.Marker({
-			position: latlng, 
-			map: map, 
-			title: "You are here! (at least within a "+ latlng.acc +" meter radius)"
-		});
-	}
-
 	$("#force-send").on("click", function(e) {
-		console.log("click");
-		var gigi = navigator.geolocation.getCurrentPosition();
+		var position = navigator.geolocation.getCurrentPosition(function(position) {   
+				player.lat = position.coords.latitude;
+				player.lng = position.coords.longitude;
+				player.acc = position.coords.accuracy;
 
-		console.log(gigi);
+				$("#map_status").html(player);
 
-		sendcoords(gigi);
+				var latlng = new google.maps.LatLng(player.lat, player.lng);
+				console.log(latlng);
+				map.setCenter(latlng);
+				setMarker(latlng, player.id);
+
+				sendCoords(player);
+			}, function(err) {
+				$("#map_status").html("Error: see console.");
+				console.dir(err);
+			}, { enableHighAccuracy: true, maximumAge: 30000, timeout: 10000 });
 	});
+
+	$("#force-center").on("click", function(e) {
+		var latlng = new google.maps.LatLng(player.lat, player.lng);
+		map.setCenter(latlng);
+	});
+
+	status.html("Connecting...");
+	init();
 
 	/* 
 	* Socket.io
 	*/
 
-	function sendCoords(coords) {
-		socket.emit("coords", { lat: coords.latitude, lon: coords.longitude, acc: coords.accuracy });
-	}
-	    
     socket.on('connect', function() {
     	status.html("Connected.");
-
-    	if (navigator.geolocation) {
-			watchid = navigator.geolocation.watchPosition(function(position) {   
-				last_coords.latitude = position.coords.latitude;
-				last_coords.longitude = position.coords.longitude;
-				last_coords.accuracy = position.coords.accuracy;
-
-				$("h1").addClass("sent");
-				setTimeout(function() {
-		    		$("h1").removeClass("sent");
-		    	}, 300);
-			
-				var latlng = new google.maps.LatLng(last_coords.latitude, last_coords.longitude);
-
-				console.log(latlng);
-
-				$("#map_status").html(last_coords.latitude +' '+ last_coords.longitude +' '+ last_coords.accuracy);
-				map.setCenter(latlng);
-				setMarker(latlng);
-
-				sendCoords(last_coords);
-			}, function(err) {
-				$("#map_status").html("Error: see console.");
-				console.dir(err);
-			}, { enableHighAccuracy: true, maximumAge: 30000, timeout: 27000 });
-		} else {
-			$("#map_status").html("Error: Unable to grab your current position.");
-		}
 	});
 			
 	socket.on('disconnect', function() {
@@ -109,6 +150,8 @@ $(document).ready(function() {
 	
 	socket.on('clientId', function(data) {
     	clientId.html(data.id);
+
+    	geolocate();
 	});
 	
 	socket.on('tot', function(data) {	
@@ -118,12 +161,93 @@ $(document).ready(function() {
 	socket.on('coords', function(data) {
 		//TODO: Received coords from XYZ
     	//$("map_status").html(data.lat +" "+ data.lon +" "+ data.acc);
-		var latlng = new google.maps.LatLng(data.latitude, data.longitude);
-    	setMarker(latlng);
+		var latlng = new google.maps.LatLng(data.lat, data.lng);
+    	setMarker(latlng, id);
 
     	$("h1").addClass("received");
     	setTimeout(function() {
     		$("h1").removeClass("received");
     	}, 300);
 	});
+
+	socket.on('join', function(data) {
+		//var player = jQuery.extend(true, {}, data);
+		player.id = data.player.id;
+		player.nick = data.player.nick;
+		player.lat = data.player.lat;
+		player.lng = data.player.lng;
+		player.acc = data.player.acc;
+
+		log('Received current player id: '+ player.id);
+		log('You have joined the server.');
+	});
+
+	socket.on('quit', function(data) {
+		var quitter = '';
+
+		var length = players.length;
+		for(var i = 0; i < length; i++) {
+			if (players[i].id == data.id) {
+				players[i].marker.setMap(null);
+				quitter = players[i].nick;
+				players.splice(i, 1);
+				break;
+			}
+		}
+
+		log('Player quitted: '+ quitter +' (id '+ data.id +')');
+	});
+
+	socket.on('newplayer', function(data) {
+		var newPlayer = new Player();
+		newPlayer.id = data.player.id;
+		newPlayer.nick = data.player.nick;
+		newPlayer.lat = data.player.lat;
+		newplayer.lng = data.player.lng;
+		newPlayer.acc = data.player.acc;
+
+		players.push(newPlayer);
+		log('New player joined: '+ newPlayer.nick);
+		newPlayer = {};
+	});
+
+	socket.on('playerlist', function(data) {
+		players = []; //prepare for new list
+
+		var length = data.list.length;
+		for(var i = 0; i < length; i++) {
+			var tmpPlayer = new Player();
+			tmpPlayer.id = data.list[i].id;
+			tmpPlayer.nick = data.list[i].nick;
+			tmpPlayer.lat = data.list[i].lat;
+			tmpplayer.lng = data.list[i].y;
+			tmpPlayer.acc = data.list[i].acc;
+			tmpPlayer.ping = data.list[i].ping;
+
+			players.push(tmpPlayer);
+			tmpPlayer = {};
+		}
+
+		log('Initial player list received: '+ length +' players.');
+	});
+
+	socket.on('ping', function(data) {
+		socket.emit('pong', { time: Date.now() });
+		//log('Ping? Pong!');
+	});
+
+	socket.on('pingupdate', function(data) {
+		var length = players.length;
+		for(var i = 0; i < length; i++) {
+			if (players[i].id == data.id) {
+				players[i].ping = data.ping;
+				if (player.id == data.id) {
+					player.ping = data.ping;
+					//$("#ping").html(player.ping +'ms');
+				}
+				break;
+			}
+		}
+	});
+
 });
--- server.js
+++ server.js
@@ -40,29 +40,121 @@ console.log("* Express server listening in %s mode", app.settings.env);
 */
 
 var	io = require('socket.io').listen(app),
- 	totUsers = 0;
+	Player = require('./public/js/Player.js').Player,
+	players = [],
+ 	totPlayers = 0,
+	pings = [],
+	pingInterval = null,
+	pingEvery = 5000;
 	
 io.configure(function() { 
 	io.enable('browser client minification');
 	io.set('log level', 1); 
-}); 
+});
+
+function getPlayerById(id) {
+	var length = players.length;
+	for(var i = 0; i < length; i++) {
+		if (players[i].id == id) {
+			return players[i];
+		}
+	}
+}
+
+function newPlayer(client) {
+	p = new Player(client.id);
+	players.push(p);
+
+	client.emit('join', { player: p });
+	client.broadcast.emit('newplayer', { player: p });
+
+	console.log('+ New player: '+ p.nick);
+}
+
+function sendPlayerList(client) {
+	client.emit('playerlist', { list: players }); //FIXME improve me
+	console.log('* Sent player list to '+ client.id);
+}
+
+// ping is intended as server -> client -> server time
+function pingClients() {
+	var length = players.length;
+	for(var i = 0; i < length; i++) {
+		if (players[i].id) {
+			pings[players[i].id] = { time: Date.now(), ping: 0 };
+			//console.log('Ping? '+ players[i].id); //log filler
+			io.sockets.sockets[players[i].id].emit('ping');
+		}
+	}
+}
 
 io.sockets.on('connection', function(client) {
-	totUsers++;
-	console.log('+ User '+ client.id +' connected, total users: '+ totUsers);
+	newPlayer(client);
+	sendPlayerList(client);
+
+	totPlayers++;
+	console.log('+ User '+ client.id +' connected, total users: '+ totPlayers);
 
 	client.emit("clientId", { id: client.id });
-	io.sockets.emit("tot", { tot: totUsers });
+	io.sockets.emit("tot", { tot: totPlayers });
+
+	if ((totPlayers == 1) && (pingInterval === null)) {
+		pingInterval = setInterval(pingClients, pingEvery);
+	}
 
 	client.on("coords", function(data) {		
-		//console.dir(data);
+		console.dir(data);
+
+		var length = players.length;
+		for(var i = 0; i < length; i++) {
+			if (players[i].id == data.id) {
+				players[i].lat = data.lat;
+				players[i].lng = data.lng;
+				players[i].acc = data.acc;
+				break;
+			}
+		}
 		
-		io.sockets.emit("coords", { lat: data.latitude, lon: data.longitude, acc: data.accuracy });
+		client.broadcast.emit("coords", { lat: data.latitude, lon: data.longitude, acc: data.accuracy });
+	});
+
+	client.on('pong', function(data) {
+		pings[client.id] = { ping: (Date.now() - pings[client.id].time) };
+
+		var length = players.length;
+		for(var i = 0; i < length; i++) {
+			if (players[i].id == client.id) {
+				players[i].ping = pings[client.id].ping;
+				break;
+			}
+		}
+
+		//console.log('Pong! '+ client.id +' '+ pings[client.id].ping +'ms'); //log filler
+
+		//broadcast confirmed player ping
+		io.emit('pingupdate', { id: client.id, ping: pings[client.id].ping });
 	});
 
 	client.on('disconnect', function() {
-		totUsers--;
-		console.log('- User '+ client.id +' disconnected, total users: '+ totUsers);
-		io.sockets.emit("tot", { tot: totUsers });
+		var quitter = '';
+
+		var length = players.length;
+		for(var i = 0; i < length; i++) {
+			if (players[i].id == client.id) {
+				quitter = players[i].nick;
+				players.splice(i, 1);
+				break;
+			}
+		}
+
+		totPlayers--;
+		client.broadcast.emit('quit', { id: client.id });
+		io.sockets.emit('tot', { tot: totPlayers });
+		console.log('- Player '+ quitter +' ('+ client.id +') disconnected, total players: '+ totPlayers);
+
+		if (totPlayers == 0) {
+			clearTimeout(pingInterval);
+			pingInterval = null;
+		}
 	});
 });
